40e1aff900013371706446cf088ac7fd887b1c9e
Priyanka-SA04 <priyankasa2092004@gmail.com>
2025-09-17 22:01:23 +0530

    initial settings done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import { Button } from './components/ui/button'
+
+function App() {
+
+  return (
+    <div>
+      <Button>Login</Button>
+      
+    </div>
+  )
+}
+
+export default App
