863faa0708cf9a1b5992025e463893678c463f9c
Priyanka-SA04 <priyankasa2092004@gmail.com>
2025-09-17 23:13:32 +0530

    Page routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,55 @@
 
-import './App.css'
-import { Button } from './components/ui/button'
+import AppLayout from './layouts/AppLayout'
+import JobListing from './pages/JobListing'
+import JobPage from './pages/JobPage'
+import LandingPage from './pages/LandingPage'
+import Onboarding from './pages/Onboarding'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import PostJob from './pages/PostJob'
+import SavedJobs from './pages/SavedJobs'
+import MyJobs from './pages/MyJobs'
 
-function App() {
+const router=createBrowserRouter([
+  {
+    element:<AppLayout/>,
+    children:[
+      {
+        path:"/",
+        element:<LandingPage/>,
+      },
+      {
+        path:"/onboarding",
+        element:<Onboarding/>,
+      },
+      {
+        path:"/jobs",
+        element:<JobListing/>,
+      },
+      {
+        path:"/job/:id",
+        element:<JobPage/>
+      },
+      {
+        path:"/post-job",
+        element:<PostJob/>,
+      },
+      {
+        path:"/saved-jobs",
+        element:<SavedJobs/>,
+      },
+      {
+        path:"/my-jobs",
+        element:<MyJobs/>,
+      }
+    ]
+  }
+])
 
+function App() 
+{
   return (
-    <div>
-      <Button>Login</Button>
-      
-    </div>
+    <RouterProvider router={router}/>
   )
 }
 
-export default App
+export default App;
